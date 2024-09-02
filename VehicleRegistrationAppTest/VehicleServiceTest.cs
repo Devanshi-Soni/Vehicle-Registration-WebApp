a9028eff2d38de772f83087d3ae6123922664975
Devanshi_kadecha <141355756+Devanshi-Soni@users.noreply.github.com>
2024-09-02 22:35:57 +0530

    Add unit test for vehicle service

--- VehicleRegistrationAppTest/VehicleServiceTest.cs
+++ VehicleRegistrationAppTest/VehicleServiceTest.cs
@@ -14,7 +14,6 @@ namespace VehicleRegistrationAppTest
     public class VehicleServiceTest
     {
         private readonly Mock<ApplicationDbContext> _dbContext;
-        private readonly Mock<IUserService> _userService;
         private readonly Mock<IVehicleService> _vehicleService;
         private readonly IFixture _fixture;
         public VehicleServiceTest()
@@ -26,42 +25,37 @@ namespace VehicleRegistrationAppTest
             ApplicationDbContext dbContext = dbContextMock.Object;
 
             // inital data for DB by using mock 
-            var usersInitialData = new List<UserModel>() { };
             var vehiclesInitialData = new List<VehicleModel>() { };
-            
             //mocks for DbSet 
             dbContextMock.CreateDbSetMock(temp => temp.VehiclesDetails, vehiclesInitialData); 
-            dbContextMock.CreateDbSetMock(temp => temp.Users, usersInitialData);
-
             //Create services based on mocked DbContext object
-            _userService = new Mock<IUserService>(dbContext);
             _vehicleService = new Mock<IVehicleService>(dbContext);
 
             // fixture object for Creating vehicle data 
-            var vehicle = _fixture.Create<VehicleModel>();
+            var vehicle = _fixture.Build<VehicleModel>()
+                                .Without(v => v.User) 
+                                .With(v => v.VehicleId, Guid.NewGuid())
+                                .With(v => v.VehicleNumber, "ABC123")
+                                .With(v => v.VehicleOwnerName, "John Doe")
+                                .With(v => v.OwnerContactNumber, "123-456-7890")
+                                .With(v => v.VehicleClass, "Sedan")
+                                .With(v => v.FuelType, "Gasoline")
+                                .With(v => v.UserId, 1)
+                                .Create();
             _vehicleService.Setup(v => v.GetVehicleByIdAsync(It.IsAny<Guid>())).ReturnsAsync(vehicle);
             
         }
 
         #region Add Vehicle
 
-        // when the vehicle add request is null it returns Argument Null Exception 
-        [Fact]
-        public async Task AddVehicle_WithNullData()
-        {
-            //Arrange
-            VehicleModel vehicle = null;
-
-            //Assert
-            await Assert.ThrowsAsync<ArgumentNullException>(() => _vehicleService.Object.AddVehicle(vehicle));
-        }
-
-        //when vehicle add request is with proper details it add vehicle and returns success 
+        // Test for Adding a Vehicle with Valid Data 
         [Fact]
         public async Task AddVehicle_WithProperData()
         {
             // Arrange
-            var vehicle = _fixture.Create<VehicleModel>();
+             var vehicle = _fixture.Build<VehicleModel>()
+                      .Without(v => v.User) 
+                      .Create();
             _vehicleService.Setup(v => v.AddVehicle(It.IsAny<VehicleModel>())).ReturnsAsync(vehicle);
             
             //Act
@@ -72,6 +66,118 @@ namespace VehicleRegistrationAppTest
             Assert.Equal(vehicle.VehicleId, result.VehicleId);
         }
         #endregion
+        
+        #region Edit Vehicle
 
+         // Test for Editing a Vehicle with Valid Data
+         [Fact]
+         public async Task EditVehicle_WithProperData()
+         {
+            // Arrange
+            var vehicle = _fixture.Build<VehicleModel>()
+                               .Without(v => v.User) 
+                               .Create();
+            var userId = vehicle.UserId.ToString();
+
+            _vehicleService.Setup(v => v.EditVehicle(It.IsAny<VehicleModel>(), userId)).ReturnsAsync(vehicle);
+        
+             // Act
+             var result = await _vehicleService.Object.EditVehicle(vehicle, userId);
+        
+             // Assert
+             Assert.NotNull(result);
+             Assert.Equal(vehicle.VehicleId, result.VehicleId);
+         }
+        
+         // Test for Editing a Vehicle that does not exist
+         [Fact]
+         public async Task EditVehicle_VehicleNotFound()
+         {
+            // Arrange
+            var vehicle = _fixture.Build<VehicleModel>()
+                                .Without(v => v.User) 
+                                .Create();
+            var userId = vehicle.UserId.ToString();
+
+            _vehicleService.Setup(v => v.EditVehicle(It.IsAny<VehicleModel>(), userId))
+                            .ThrowsAsync(new NullReferenceException("Vehicle not found."));
+        
+             // Assert
+             await Assert.ThrowsAsync<NullReferenceException>(() => _vehicleService.Object.EditVehicle(vehicle, userId));
+         }
+        #endregion
+
+        #region Delete Vehicle
+
+        // Test for Deleting a Vehicle that does not exist
+        [Fact]
+        public async Task DeleteVehicle_VehicleNotFound()
+        {
+            // Arrange
+            var vehicleId = Guid.NewGuid();
+            _vehicleService.Setup(v => v.DeleteVehicle(vehicleId)).ReturnsAsync((VehicleModel)null);
+        
+            // Act
+            var result = await _vehicleService.Object.DeleteVehicle(vehicleId);
+        
+            // Assert
+            Assert.Null(result);
+        }
+        
+        // Test for Deleting a Vehicle with Valid Data
+        [Fact]
+        public async Task DeleteVehicle_WithProperData()
+        {
+            // Arrange
+            var vehicleId = Guid.NewGuid();
+            _vehicleService.Setup(v => v.DeleteVehicle(vehicleId)).ReturnsAsync((VehicleModel)null);
+
+            // Act
+            var result = await _vehicleService.Object.DeleteVehicle(vehicleId);
+
+            // Assert
+            Assert.Null(result);
+        }
+        #endregion
+        
+        #region Get Vehicle By Id
+
+        // Test for Getting Vehicle By Id with Valid Id
+        [Fact]
+        public async Task GetVehicleById_WithProperId()
+        {
+            // Arrange
+            var vehicle = _fixture.Build<VehicleModel>()
+                                 .Without(v => v.User) 
+                                 .Create();
+            var vehicleId = vehicle.VehicleId;
+
+            _vehicleService.Setup(v => v.GetVehicleByIdAsync(vehicleId)).ReturnsAsync(vehicle);
+
+            // Act
+            var result = await _vehicleService.Object.GetVehicleByIdAsync(vehicleId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(vehicle.VehicleId, result.VehicleId);
+        }
+        
+        // Test for Getting Vehicle By Id with Invalid Id
+        [Fact]
+        public async Task GetVehicleById_WithInvalidId()
+        {
+            // Arrange
+            var vehicleId = Guid.NewGuid();
+        
+            _vehicleService.Setup(v => v.GetVehicleByIdAsync(vehicleId)).ReturnsAsync((VehicleModel)null);
+        
+            // Act
+            var result = await _vehicleService.Object.GetVehicleByIdAsync(vehicleId);
+        
+            // Assert
+            Assert.Null(result);
+        }
+        
+        #endregion
     }
 }
